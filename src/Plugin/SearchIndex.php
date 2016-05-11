4f6cee7c32e7143746cddd8dd6bd518e9feead44
karthick <karthick.srinivasan@ameexusa.com>
2016-05-11 15:28:40 +0530

    text changed

--- src/Plugin/SearchIndex.php
+++ src/Plugin/SearchIndex.php
@@ -8,32 +8,37 @@ use Drupal\Core\Entity\EntityManagerInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Render\RendererInterface;
 
-class SearchIndex extends PluginBase{
+class SearchIndex extends PluginBase {
 
 	public function _indexNode(NodeInterface &$node) {
-	  $languages = $node->getTranslationLanguages();
+	  $languages   = $node->getTranslationLanguages();
 	  $node_render = \Drupal::entityManager()->getViewBuilder('node');
 	  foreach ($languages as $language) {
 	    $node = $node->getTranslation($language->getId());
+
 	    // Render the node.
 	    $build = $node_render->view($node, 'search_index', $language->getId());
 	    unset($build['#theme']);
+
 	    // Add the title to text so it is searchable.
 	    $build['search_title'] = [
-	      '#prefix' => '<h1>',
+	      '#prefix'     => '<h1>',
 	      '#plain_text' => $node->label(),
-	      '#suffix' => '</h1>',
-	      '#weight' => -1000
+	      '#suffix'     => '</h1>',
+	      '#weight'     => -1000
 	    ];
-	    $text =  \Drupal::service('renderer')->renderPlain($build);
+	    $text = \Drupal::service('renderer')->renderPlain($build);
+
 	    // Fetch extra data normally not visible.
 	    $extra = \Drupal::moduleHandler()->invokeAll('node_update_index', [$node]);
 	    foreach ($extra as $t) {
 	      $text .= $t;
 	    }
+
 	    // Update index, using search index "type" equal to the plugin ID.
-      	// "node_search" is a static plugin ID for all indexNode.
+	  	// "node_search" is a static plugin ID for all indexNode.
 	    search_index(node_search, $node->id(), $language->getId(), $text);
 	  }
 	}
+	
 }
